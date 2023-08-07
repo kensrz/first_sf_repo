be5abf480d29290d9f0b63223cdefabcb8c267be
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:25:43 +0800

    Update sf_streamlit_app.py

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -7,3 +7,14 @@ streamlit.write('Oatmeal and Milk')
 st.divider()
 streamlit.write('Pancake Waffles)
 st.divider()
+
+
+st.write("This is some text.")
+
+st.slider("This is a slider", 0, 100, (25, 75))
+
+st.divider()  # 👈 Draws a horizontal rule
+
+st.write("This text is between the horizontal rules.")
+
+st.divider()  # 👈 Another horizontal rule
