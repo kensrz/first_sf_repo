936cdf68e93dc5afea3d56959b5cd1e4f71be68a
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:07:06 +0800

    Update sf_streamlit_app.py

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -4,13 +4,4 @@ import numpy
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.write(
-lst = ['a', 'b', 'c']
-
-s = ''
-
-for i in lst:
-    s += "- " + i + "\n"
-
-st.markdown(s)
-)
+streamlit.write('sample text')
