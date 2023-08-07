6751774365ba0f2b95ccae73d3300424f7317870
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:45:38 +0800

    Update sf_streamlit_app.py
    practice codes

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -1,6 +1,11 @@
-import streamlit
+import streamlit as st
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Oatmeal and Milk')
+st.divider()
+st.text('Oatmeal and Almond Milk')
+st.divider()
+st.text('Non-pork Bacon and Synth-Eggs')
+st.divider()
+st.text('Tossed Vegatables')
 st.divider()
