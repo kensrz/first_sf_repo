b43f35e93bc8a82e1b8e50a3c2a292b1ead98330
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:35:50 +0800

    Update sf_streamlit_app.py

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.write('Oatmeal and Milk \n Pancake and Waffles')
+streamlit.write('Oatmeal and Milk'\n)
+streamlit.write('Pancake and Waffles'\n)
