95e007956077b165705625429dfc302b9894b478
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:48:14 +0800

    Update sf_streamlit_app.py

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -1,6 +1,7 @@
-import streamlit as st
+import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Oatmeal and Almond Milk')
 streamlit.text('Non-pork Bacon and Synth-Eggs')
