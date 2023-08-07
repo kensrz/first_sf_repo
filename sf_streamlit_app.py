cb2a157591974d5c5640e8656b4730a68dba89ea
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:17:14 +0800

    Update sf_streamlit_app.py

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -1,13 +1,8 @@
-import streamlit as st
-import pandas
-import numpy
+import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-
 streamlit.write('Oatmeal and Milk')
-
 streamlit.write('Non-pork Bacon and Synthetic Eggs')
-
 streamlit.write('Disappointing Vegetable Salad')
 
