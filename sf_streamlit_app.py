e5c424e40419800fc3bc5456773164d8f64f951c
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:24:48 +0800

    Update sf_streamlit_app.py

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -4,4 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.write('Oatmeal and Milk')
 
-
+st.divider()
+streamlit.write('Pancake Waffles)
+st.divider()
