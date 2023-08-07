5a93aa00421dfb84fdabdafc809a4742fc78b086
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 16:01:44 +0800

    Update sf_streamlit_app.py
    Finishing touches

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.divider()
 streamlit.text('Oatmeal and Almond Milk')
+streamlit.divider()
 streamlit.text('Non-pork Bacon and Synth-Eggs')
+streamlit.divider()
 streamlit.text('Tossed Vegatables')
+streamlit.divider()
