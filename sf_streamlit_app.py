7c60b2c9cbf448053d600e58ed6a4793223592fc
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-07 15:13:25 +0800

    Update sf_streamlit_app.py

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -6,8 +6,8 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.write('Oatmeal and Milk')
-st.divider()
+
 streamlit.write('Non-pork Bacon and Synthetic Eggs')
-st.divider()
+
 streamlit.write('Disappointing Vegetable Salad')
-st.divider()
+
