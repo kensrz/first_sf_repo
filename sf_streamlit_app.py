fee83bdf56ffc4bfc7cbc0e40bdff7511c8d4462
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-09 09:59:55 +0800

    Update sf_streamlit_app.py
    removed raw json text

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -28,9 +28,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes data onscreen
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # normalize json from response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display normalized json as dataframe
