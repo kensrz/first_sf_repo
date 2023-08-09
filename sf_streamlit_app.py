d1fdd45cedaa1f27c85fda9c66a02d6ed39ae441
kensrz <141610961+kensrz@users.noreply.github.com>
2023-08-09 14:34:15 +0800

    Update sf_streamlit_app.py
    restructure fruityvyce section and nested if

--- sf_streamlit_app.py
+++ sf_streamlit_app.py
@@ -30,15 +30,16 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api advice
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# normalize json from response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display normalized json as dataframe
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)ruit
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #dont run anything here until i fix it
 streamlit.stop()
